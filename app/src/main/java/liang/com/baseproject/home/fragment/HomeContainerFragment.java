4f6e8a2a006a8788ebf6a8f7fb6b6f2309ce8c65
1935990239@qq.com <11935990239Smile>
2019-09-10 20:09:10 +0800

    [develop] 主界面Activity保存SharedPreferences时效性，在父Activity的子Fragment的onResume()方法中调用SPUtils方法，保证实时切换主题颜色

--- app/src/main/java/liang/com/baseproject/home/fragment/HomeContainerFragment.java
+++ app/src/main/java/liang/com/baseproject/home/fragment/HomeContainerFragment.java
@@ -2,16 +2,13 @@ package liang.com.baseproject.home.fragment;
 
 
 import android.content.Context;
-import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
-import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Gravity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
@@ -21,14 +18,11 @@ import com.scwang.smartrefresh.layout.api.RefreshLayout;
 import com.scwang.smartrefresh.layout.listener.OnLoadMoreListener;
 import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 import butterknife.BindView;
 import butterknife.Unbinder;
 import liang.com.baseproject.R;
-import liang.com.baseproject.main.activity.AgentWebActivity;
 import liang.com.baseproject.activity.MainHomeActivity;
 import liang.com.baseproject.base.MVPBaseFragment;
 import liang.com.baseproject.home.adapter.HomeContainerAdapter;
@@ -36,6 +30,7 @@ import liang.com.baseproject.home.entity.ArticleBean;
 import liang.com.baseproject.home.entity.HomeBean;
 import liang.com.baseproject.home.presenter.HomeContainerPresenter;
 import liang.com.baseproject.home.view.HomeContainerView;
+import liang.com.baseproject.main.activity.AgentWebActivity;
 import liang.com.baseproject.utils.JsonFormatUtils;
 import liang.com.baseproject.utils.LogUtil;
 import liang.com.baseproject.utils.ToastUtil;
@@ -174,6 +169,7 @@ public class HomeContainerFragment extends MVPBaseFragment<HomeContainerView, Ho
     @Override
     public void onResume() {
         super.onResume();
+        getSmartRefreshPrimaryColorsTheme(smartRefreshLayout, true, true);
         LogUtil.d(TAG, "执行onResume()");
     }
 
@@ -250,4 +246,5 @@ public class HomeContainerFragment extends MVPBaseFragment<HomeContainerView, Ho
     public void onHideProgress() {
 
     }
+
 }
