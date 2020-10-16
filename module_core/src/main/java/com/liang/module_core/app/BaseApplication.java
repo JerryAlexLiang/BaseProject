264df251e28b37900a9e3aad01da1b92fb84345f
yangliang <yangliang@fosun.com>
2020-10-16 17:39:19 +0800

    [develop] 一些测试调整

--- module_core/src/main/java/com/liang/module_core/app/BaseApplication.java
+++ module_core/src/main/java/com/liang/module_core/app/BaseApplication.java
@@ -17,6 +17,7 @@ import androidx.multidex.MultiDex;
 import androidx.multidex.MultiDexApplication;
 
 import com.alibaba.android.arouter.launcher.ARouter;
+import com.liang.module_core.BuildConfig;
 import com.liang.module_core.utils.SettingUtils;
 import com.liang.module_core.utils.ToastUtil;
 import com.liang.module_core.utils.Utils;
@@ -104,10 +105,10 @@ public class BaseApplication extends MultiDexApplication {
      * 初始化路由SDK
      */
     private void initARouter() {
-//        if (BuildConfig.DEBUG) {  // 这两行必须写在init之前，否则这些配置在init过程中将无效
-//            ARouter.openLog();   // 打印日志
-//            ARouter.openDebug(); // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
-//        }
+        if (BuildConfig.DEBUG) {  // 这两行必须写在init之前，否则这些配置在init过程中将无效
+            ARouter.openLog();   // 打印日志
+            ARouter.openDebug(); // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
+        }
         // 尽可能早，推荐在Application中初始化
         ARouter.init(app);
     }
