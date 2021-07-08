f16217f333d55199bcd93021c623d93e7bf8d74a
yangliang123 <1935990239@qq.com>
2021-07-08 10:57:03 +0800

    [develop] 完善BasePresenter

--- module_core/src/main/java/com/liang/module_core/mvp/MVPBasePresenter.java
+++ module_core/src/main/java/com/liang/module_core/mvp/MVPBasePresenter.java
@@ -10,25 +10,33 @@ import java.lang.ref.WeakReference;
  */
 public abstract class MVPBasePresenter<V> {
 
+    //1、将Presenter对View(Activity)的引用使用弱引用(WeakReference)的方式
+    //View接口类型弱应用
     protected Reference<V> mViewRef;
 
     public void attachView(V view) {
+        //创建弱引用
         mViewRef = new WeakReference<V>(view);
     }
 
     protected V getView() {
-        return mViewRef.get();
+        if (mViewRef != null) {
+            return mViewRef.get();
+        }
+        return null;
     }
 
     public boolean isViewAttached() {
         return mViewRef != null && mViewRef.get() != null;
     }
 
+    //解除关联
+    //结合Activity的生命周期去进行attachView和detachView,接触Presenter层与View层的关联
     public void detachView() {
         if (mViewRef != null) {
+            //在View销毁的时候，手动GC，让系统回收内存
             mViewRef.clear();
             mViewRef = null;
-
         }
     }
 }
